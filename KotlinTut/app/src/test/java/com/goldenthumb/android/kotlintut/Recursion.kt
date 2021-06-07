4492306af67de03406a96d53333ec72800b4b3d9
Golden Thumb <zhijunsheng@mbp2020.local>
2021-06-07 11:29:40 -0400

    added code for fibonacci with recursion

--- KotlinTut/app/src/test/java/com/goldenthumb/android/kotlintut/Recursion.kt
+++ KotlinTut/app/src/test/java/com/goldenthumb/android/kotlintut/Recursion.kt
@@ -5,6 +5,20 @@ import org.junit.Assert.*
 
 class Recursion {
 
+    private fun fibo(n: Int): Int {
+        if (n < 2) {
+            return n
+        }
+        return fibo(n - 1) + fibo(n - 2)
+    }
+
+    @Test
+    fun printFibos() {
+        for (i in 0..10)
+            print("${fibo(i)} ")
+        assertEquals(55, fibo(10))
+    }
+
     private fun factorial(n: Int): Int {
         if (n == 0) return 1
         return n * factorial(n - 1)
