c1ad938ec78fadd93be4dd0e0c26fdad666a6a23
Golden Thumb <zhijunsheng@mbp2020.local>
2021-06-07 18:12:25 -0400

    created SwapTwoNumbers.kt to play with swapping 2 numbers

---
+++ KotlinTut/app/src/test/java/com/goldenthumb/android/kotlintut/SwapTwoNumbers.kt
@@ -0,0 +1,23 @@
+package com.goldenthumb.android.kotlintut
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class SwapTwoNumbers {
+
+    @Test
+    fun swapTwoNumbers() {
+        var a = 3
+        var b = 5
+        assertEquals(3, a)
+        assertEquals(5, b)
+
+        // here is our swapping code
+        a += b
+        b = a - b
+        a -= b
+
+        assertEquals(5, a)
+        assertEquals(3, b)
+    }
+}
